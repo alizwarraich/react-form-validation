e471fa6d2d46cf6f3a57c6c04a796364d40ccd71
Ali Zulqarnain <warraichleopard@gmail.com>
2022-08-29 14:10:50 +0500

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import FormInput from './components/FormInput';
-import { inputs } from './data/InputData';
 
 function App() {
   const [user, setUser] = useState({
@@ -11,6 +10,56 @@ function App() {
     confirmpassword: '',
     dob: '',
   });
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username',
+      errormessage: 'Username should be 3-16 characters and should not include any special character!',
+      label: 'Username',
+      required: true,
+      pattern: '^[a-zA-Z0-9]{3,16}$'
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'Email',
+      errormessage: 'It should be a valid email address!',
+      required: true,
+      label: 'Email'
+    },
+    {
+      id: 3,
+      name: 'password',
+      type: 'text',
+      placeholder: 'Password',
+      errormessage: 'Password should be 8-10 characters and should include at least one letter, one number, and one special character!',
+      required: true,
+      label: 'Password',
+      pattern: '^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,10}$'
+    },
+    {
+      id: 4,
+      name: 'confirmpassword',
+      type: 'text',
+      placeholder: 'Retype Password',
+      errormessage: `Passwords don't match!`,
+      required: true,
+      label: 'Confirm Password',
+      pattern: user.password
+    },
+    {
+      id: 5,
+      name: 'dob',
+      type: 'date',
+      placeholder: 'Birthday',
+      errormessage: '',
+      required: false,
+      label: 'Birthday'
+    },
+  ]
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   }
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import './FormInput.css';
 
 const FormInput = (props) => {
-    const { label, onChange, id, ...inputProps } = props;
+    const [focused, setFocused] = React.useState(false);
+    const { label, errormessage, onChange, id, ...inputProps } = props;
+    const handleFocus = (e) => {
+        setFocused(true);
+    }
     return (
         <div className='formInput'>
             <label>{label}: </label>
-            <input {...inputProps} onChange={props.onChange} />
+            <input
+                {...inputProps} onChange={props.onChange} onBlur={handleFocus}
+                focused={focused.toString()} onFocus={() => {
+                    inputProps.name === "confirmpassword" && setFocused(true)
+                }
+                } />
+            <span>{errormessage}</span>
         </div>
     )
 }
