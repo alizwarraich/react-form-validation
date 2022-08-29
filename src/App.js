f2bc70c7006a071f07b967b6fc1fa232e3024be7
Ali Zulqarnain <warraichleopard@gmail.com>
2022-08-29 11:44:59 +0500

    Added UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FormInput from './components/FormInput';
 
 function App() {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    dob: '',
+  });
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <FormInput
+          type="text" name="username" field="Username" placeholder="username"
+          user={user} handleChange={handleChange} />
+        <FormInput
+          type="email" name="email" field="Email" placeholder="email"
+          user={user} handleChange={handleChange} />
+        <FormInput
+          type="password" name="password" field="Password" placeholder="password"
+          user={user} handleChange={handleChange} />
+        <FormInput
+          type="date" name="dob" field="Date of Birth" placeholder="dob"
+          user={user} handleChange={handleChange} />
+      </form>
     </div>
   );
 }
